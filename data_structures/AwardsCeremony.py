8b7ac36330fb6c0a25d53d7d84c127032f195c88
Javier Francisco Cuadra <32556567+javiercuadra@users.noreply.github.com>
2023-10-10 16:10:03 -0500

    Asjust Award Ceremony and Award Objects - Include private fields - adjust functions to use private fields and getter functions - adjust output to print content the way the instructor wants

--- data_structures/AwardsCeremony.py
+++ data_structures/AwardsCeremony.py
@@ -1,45 +1,42 @@
 class AwardsCeremony:
-  def __init__(self, name,location, start_time, end_time, hosts, awards):
-    self.name = name
-    self.location = location
-    self.start_time = start_time
-    self.end_time = end_time
-    self.hosts = hosts
-    self.awards = awards
+  def __init__(self, name, location, start_time, end_time, hosts, awards):
+    self.__name = name
+    self.__location = location
+    self.__start_time = start_time
+    self.__end_time = end_time
+    self.__hosts = hosts
+    self.__awards = awards
 
   def __str__(self):
-    output = f"Host: {self.hosts}\n"
-    for award in self.awards:
-      output += f"Award: {award.name}\n"
-      output += f"Presenters: {', '.join(award.presenters)}\n"
-      output += f"Nominees: \"{', '.join(award.nominees)}\"\n"
-      output += f"Winner: \"{award.winner}\"\n"
+    output = f"Host: {', '.join(self.get_hosts()).title()}\n\n"
+    for award in self.get_awards():
+      output += f"{award}\n"
     return output
 
   def get_name(self):
-    return self.name
+    return self.__name
 
   def get_location(self):
-    return self.location
+    return self.__location
 
   def get_start_time(self):
-    return self.start_time
+    return self.__start_time
 
   def get_end_time(self):
-    return self.end_time
+    return self.__end_time
 
   def get_hosts(self):
-    return self.hosts
+    return self.__hosts
 
   def get_awards(self):
-    return self.awards
+    return self.__awards
 
   def to_json(self):
     ceremony_dict = {
-        "Host": self.hosts,
+        "Host": [host.title() for host in self.get_hosts()],
     }
 
-    for award in self.awards:
-        ceremony_dict[award.name] = award.to_json()
+    for award in self.get_awards():
+        ceremony_dict[award.get_name().title()] = award.to_json()
 
     return ceremony_dict
