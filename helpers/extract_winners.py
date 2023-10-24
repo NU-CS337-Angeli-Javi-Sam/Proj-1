cfb5411f94a34015fb15b957dd47d0db2d036cac
Samdev23 <samj944@gmail.com>
2023-10-23 23:08:32 -0500

    Award names are roughly correlated with winners: Sam and Angeli

--- helpers/extract_winners.py
+++ helpers/extract_winners.py
@@ -12,20 +12,29 @@ def extract_winners (tweets):
     awards_to_winner = {}
 
     for key in awards_list.getKeys():
-        awards_to_winner[key] = {}
+        awards_to_winner[key] = SortedDict()
 
-        winner_regexes = [r"[A-Z][a-zA-Z\s]+ (i?)(Best Director)"]
+        winner_regexes = [r"[A-Z][a-zA-Z\s]+"]
         # print(winner_regexes[0])
 
         for tweet in tweets:
-            award_matches = re.findall(winner_regexes[0], tweet.get_original_text())
-            print(award_matches)
+            # award_matches = re.findall(winner_regexes[0], tweet.get_original_text())
+            # # print(award_matches)
+            #
+            # for award_match in award_matches:
+            if key in tweet.get_original_text():
+                name_matches = re.findall(r"[A-Z][a-zA-Z\s]*[A-Z][a-z]*",tweet.get_original_text().replace(key, ' '))
 
-            for award_match in award_matches:
-                print('hello', award_match)
-                name_matches = re.findall(r'[A-Z][a-zA-Z\\s]+',award_match.replace(key, ' '))
+                for name in name_matches:
+                    if name in awards_to_winner[key]:
+                       awards_to_winner[key].updateKV_Pair(name, awards_to_winner[key].get(name) + 1)
+                    else:
+                        if name == 'RT' or name == 'GoldenGlobes':
+                            continue
+                        awards_to_winner[key].add(name, 1)
 
-                print(str(award_match),name_matches)
+    for k, v in awards_to_winner.items():
+        print(k, v.getTop(5))
 
     # print(awards_to_winner)
 
